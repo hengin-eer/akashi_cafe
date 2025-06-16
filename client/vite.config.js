43108a0280d319a6ccc220e8e080f9df21936e74
timdaik <hirokix0608@gmail.com>
2025-06-16 17:59:50 +0900

    🔧 #6 ポート番号3000で開発サーバーを立てるように設定変更

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,30 +3,33 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		workspace: [
-			{
-				extends: './vite.config.js',
-				plugins: [svelteTesting()],
-				test: {
-					name: 'client',
-					environment: 'jsdom',
-					clearMocks: true,
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-					setupFiles: ['./vitest-setup-client.js']
-				}
-			},
-			{
-				extends: './vite.config.js',
-				test: {
-					name: 'server',
-					environment: 'node',
-					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
-				}
-			}
-		]
-	}
+  server: {
+    port: 3000,
+  },
+  plugins: [sveltekit()],
+  test: {
+    workspace: [
+      {
+        extends: './vite.config.js',
+        plugins: [svelteTesting()],
+        test: {
+          name: 'client',
+          environment: 'jsdom',
+          clearMocks: true,
+          include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+          exclude: ['src/lib/server/**'],
+          setupFiles: ['./vitest-setup-client.js']
+        }
+      },
+      {
+        extends: './vite.config.js',
+        test: {
+          name: 'server',
+          environment: 'node',
+          include: ['src/**/*.{test,spec}.{js,ts}'],
+          exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+        }
+      }
+    ]
+  }
 });
